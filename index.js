d7b52e2a2aee29b720d9eb67072d3a1d4b4bb7d4
pierasiqueira <pierasiqueira@gmail.com>
2025-04-15 14:47:37 -0300

    listener

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const inputItem = document.getElementById('input-item');
 
+const botaoAdicionar = document.getElementById('adicionar-item');
+
+botaoAdicionar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    console.log(inputItem.value);
+
+})
